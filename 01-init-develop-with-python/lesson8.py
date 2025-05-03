b95c689b5a7ba45155002c5c9bba88a6f5ece61f
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-03 14:32:09 -0300

    solution to the exercise on variables and data types

---
+++ 01-init-develop-with-python/lesson8.py
@@ -0,0 +1,12 @@
+name = 'John'
+last_name = 'Doe'
+age = 34
+birthyear = 2025 - age
+is_of_age = age >= 18
+height_in_meters = 1.75
+print('Name:', name)
+print('Last Name:', last_name)
+print('Age:', age)
+print('birthyear:', birthyear)
+print('is of age:', is_of_age)
+print('height in meters:', height_in_meters)

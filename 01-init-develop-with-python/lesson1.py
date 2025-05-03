a2b9b809ddce81df2ad403c0839bb0d516433234
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-03 12:47:49 -0300

    docstrings with comments

--- 01-init-develop-with-python/lesson1.py
+++ 01-init-develop-with-python/lesson1.py
@@ -1,2 +1,13 @@
 # allow writing to the file
+'''
+This is a comment
+This is a comment
+This is a comment
+'''
+
+"""
+This is a comment
+This is a comment
+This is a comment
+"""
 print("my first python program")

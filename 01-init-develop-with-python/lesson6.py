6515e29396631694ee1f16edba44e4e135615441
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-03 14:13:15 -0300

    conversion type

---
+++ 01-init-develop-with-python/lesson6.py
@@ -0,0 +1,14 @@
+# conversion of types, casting
+# type conversion, casting, type coercion
+#  its act of converting a value from one type to another
+# imutable types and primitives:
+# int, float, bool, str
+
+print(1 + 1)  # 2
+print('a' + 'b')  # ab
+print(int('1') + 1)  # 2
+print(type(float('1') + 1))  # <class 'float'> 2.0
+print(bool(' ')) # True
+print(bool('')) # False
+print(bool(0)) # False
+print(bool(1)) # True

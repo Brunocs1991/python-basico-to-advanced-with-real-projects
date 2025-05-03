bb855cf5c54b6998d9efa2f188ecf3af04ef0d51
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-03 14:04:29 -0300

    type boolean

---
+++ 01-init-develop-with-python/lesson5.py
@@ -0,0 +1,19 @@
+# type data bool (boolean)
+# exists only two values: True and False
+# boolean values are used to represent the truth value of an expression
+
+print(10 == 10) # True
+print(10 == 20) # False
+print(10 != 20) # True
+print(10 != 10) # False
+print(10 > 20) # False
+print(10 < 20) # True
+print(10 >= 20) # False
+print(10 <= 20) # True
+print(10 > 10) # False
+print(10 < 10) # False
+print(10 >= 10) # True
+print(10 <= 10) # True
+print(10 == 10.0) # True
+print(10 == 10.1) # False
+print(10 == "10") # False

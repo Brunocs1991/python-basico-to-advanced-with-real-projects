168df588c263013d5a42941d629e5555860ae775
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-03 14:25:38 -0300

    variables

---
+++ 01-init-develop-with-python/lesson7.py
@@ -0,0 +1,16 @@
+# variables are used to save data in memory
+# PEP8: init variables woth lowercase letters, can use numbers and underline _.
+# the signal = is the assignment operator, it assigns the value on the right to the variable on the left
+# use: name_variable = expression
+
+full_name = 'John Doe'
+sum_two_plus_two = 2 + 2
+primitive_cast = int('1')
+print(primitive_cast, type(primitive_cast))
+print(full_name, sum_two_plus_two)
+
+
+name = 'John Doe'
+age = 17
+of_age = age >= 18
+print('Name:', name, 'Age:', age, 'Of Age:', of_age)

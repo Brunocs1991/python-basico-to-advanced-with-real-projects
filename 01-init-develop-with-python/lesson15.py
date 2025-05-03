a444d1c3a2602bb72df43b1209575e1dd62a723c
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-03 18:18:06 -0300

    using inpunt function

---
+++ 01-init-develop-with-python/lesson15.py
@@ -0,0 +1,9 @@
+name = input('Whats is your name? ')
+print(f'Your name is {name=}')
+
+number_1 = input('digit a number: ')
+number_2 = input('digit another number: ')
+int_number_1 = int(number_1)
+int_number_2 = int(number_2)
+print(f'Your numbers are {int_number_1=} and {int_number_2=}')
+print(f'The sum of your numbers is {int_number_1 + int_number_2}')

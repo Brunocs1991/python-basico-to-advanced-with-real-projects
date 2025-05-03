21d513e4342390cf9c87d06c57beb0a54ffb2da7
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-03 17:54:16 -0300

    f strings

---
+++ 01-init-develop-with-python/lesson13.py
@@ -0,0 +1,11 @@
+name = "Jhon Doe"
+height = 1.75
+weight = 70.0
+imc = weight / (height ** 2)
+line_1 = f'{name} has {height:.2f} m from height,'
+line_2 = f'Weight {weight:.2f} kg and you imc is'
+line_3 = f'{imc:,.2f}'
+
+print(line_1)
+print(line_2)
+print(line_3)

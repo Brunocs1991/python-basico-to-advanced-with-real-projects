c7cdaad14b4a108fcc6fcfe83e493a09863a791e
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-03 18:38:08 -0300

    if elif else

---
+++ 01-init-develop-with-python/lesson16.py
@@ -0,0 +1,9 @@
+# if  | elif | else
+
+input_data = input('Your he wants in or out? ')
+if input_data == 'in':
+    print('Welcome!')
+elif input_data == 'out':
+    print('Goodbye!')
+else:
+    print('Invalid input!')

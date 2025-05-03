e81dcd4e164036a80d365f323c281fb8ac4b24c4
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-03 17:38:50 -0300

    imc calculation lesson

---
+++ 01-init-develop-with-python/lesson12.py
@@ -0,0 +1,8 @@
+name = "John"
+height = 1.80
+weight = 95
+imc = weight / (height ** 2)
+
+print(f"Name: {name} have {height:.2f} from height,")
+print(f"Weight: {weight} Kg and you imc is")
+print(imc)

7397d210518092d9e14e6ad94bd7b6f569018479
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-03 19:05:23 -0300

    relational operators

---
+++ 01-init-develop-with-python/lesson19.py
@@ -0,0 +1,22 @@
+"""
+Operators from comparison (relational)
+OP      Meaning              Example (True)
+>       bigger               2 > 1
+>=      bigger or equals     2 >= 2
+<       minor                1 < 2
+<=      minor or equals      2 <= 2
+==      equals               'a' == 'a'
+!=      different            'a' != 'b'
+"""
+bigger = 2 > 1
+print(bigger)
+bigger_or_equals = 2 >= 2
+print(bigger_or_equals)
+minor = 1 < 2
+print(minor)
+minor_or_equals = 2 <= 2
+print(minor_or_equals)
+equals = 'a' == 'a'
+print(equals)
+different = 'a' != 'b'
+print(different)

4e78ba24c7a86dc1aca0d9b61bdf5377ca9b089a
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-03 18:58:56 -0300

    revision if / else if / else

---
+++ 01-init-develop-with-python/lesson17.py
@@ -0,0 +1,15 @@
+condition_1 = False
+condition_2 = False
+condition_3 = False
+condition_4 = False
+
+if condition_1:
+    print("Condition 1 is True")
+elif condition_2:
+    print("Condition 2 is True")
+elif condition_3:
+    print("Condition 3 is True")
+elif condition_4:
+    print("Condition 4 is True")
+else:
+    print("All conditions are False")

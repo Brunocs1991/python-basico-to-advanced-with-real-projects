a6ec3147a32edf0553e7f2c6eeddc6ea7a278f2f
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-03 13:50:40 -0300

    type str

---
+++ 01-init-develop-with-python/lesson3.py
@@ -0,0 +1,23 @@
+"""
+python = programming language
+type of typing = dynamic typing
+str -> string -> text
+Strings are textual data, which is a sequence of characters in quotation marks.
+"""
+
+# Simple quotation marks
+print('Hello World!')
+
+# double quotation marks
+print("Hello World!")
+
+# Escaping characters
+print('Hello \'World\'!')
+print("Hello \"World\"!")
+
+# r
+print(r'Hello \'World\'!')
+print(r"Hello \"World\"!")
+
+print('Hello "World"!')
+print("Hello 'World'!")

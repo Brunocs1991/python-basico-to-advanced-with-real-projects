76467c1d4eb876fa99a5df22537da610541f232c
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-03 13:59:15 -0300

    type int and flowat

---
+++ 01-init-develop-with-python/lesson4.py
@@ -0,0 +1,21 @@
+# type int and float
+# int = integer = whole number
+# the int type is used to represent whole numbers, both positive and negative.
+# int no sign = positive number
+print(11)
+print(-11)
+print(0)
+
+# float = floating point number = decimal number
+# the float type is used to represent decimal numbers, both positive and negative.
+# float no sign = positive number
+print(11.0)
+print(-11.0)
+print(0.0)
+
+
+# the function type() is used to determine the type of a variable.
+print(type(11))  # int
+print(type(11.0))  # float
+
+# inferred value

0f51c8988ab066087a4024d7247de77fee6e37bb
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-03 17:15:30 -0300

    matematical operations

---
+++ 01-init-develop-with-python/lesson9.py
@@ -0,0 +1,14 @@
+add = 10 + 10
+print('Addition:', add)
+sub = 10 - 5
+print('Subtraction:', sub)
+mul = 10 * 5
+print('Multiplication:', mul)
+div = 10 / 3
+print('Division:', div)
+whole_div = 10 // 3
+print('Whole Division:', whole_div)
+exp = 2 ** 10
+print('Exponentiation:', exp)
+mod = 10 % 3
+print('Modulus:', mod)

7494e83f49a033493d11effdb5fecf8fc81a67cc
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-04 12:46:34 -0300

    resolution to challenge

---
+++ 01-init-develop-with-python/lesson20.py
@@ -0,0 +1,13 @@
+frist_value = input('input a value: ')
+secound_value = input('input anote value: ')
+
+if frist_value >= secound_value:
+    print(
+        f'{frist_value=}  is bigger '
+        f'than {secound_value=}'
+    )
+else:
+    print(
+        f'{secound_value=} is bigger '
+        f'than {frist_value=}'
+    )

5083af8ee8ee81cd01e88c0e231ce5961947885f
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-11 12:42:23 -0300

    exercise test your knowledge so far

---
+++ 01-init-develop-with-python/lesson28.py
@@ -0,0 +1,31 @@
+"""
+    Exercise
+    Ask the user to enter their name
+    Ask the user to enter their age
+    If both name and age are entered:
+    Display:
+    Your name is {name}
+    Your name reversed is {name reversed}
+    Your name contains (or does not contain) spaces
+    Your name has {n} letters
+    The first letter of your name is {letter}
+    The last letter of your name is {letter}
+    If nothing is entered for name or age:
+    Display "Sorry, you left empty fields."
+"""
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+
+if name and age:
+    print(f'Your name is {name}')
+    print(f'Your name reversed is {name[::-1]}')
+
+    if ' ' in name:
+        print('Your name contains spaces')
+    else:
+        print('Your name does NOT contain spaces')  
+        print(f'Your name has {len(name)} letters')
+        print(f'The first letter of your name is {name[0]}')
+        print(f'The last letter of your name is {name[-1]}')
+else:
+    print("Sorry, you left some fields empty.")

d15ddfeca7daeaa8be369a5bb8bc47d1080df5bf
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-05-11 12:39:02 -0300

    slicing of strings and the len function

---
+++ 01-init-develop-with-python/lesson27.py
@@ -0,0 +1,10 @@
+"""
+String slicing
+012345678
+Hello world
+-987654321
+Slicing [i:f:p] [::]
+Note: the len function returns the number of characters in the str
+"""
+variable = 'Olá mundo'
+print(variable[::-1])
